b139c0d9dc37f2655a69f1bb0e4545e717ce2f6b
bianchi.emmanuele <bianchi.emmanuele@TAVE-PC33.OSDBSESTO.local>
2020-11-24 13:38:04 +0100

    Process Class

--- src/javathread1/Processo.java
+++ src/javathread1/Processo.java
@@ -9,14 +9,16 @@ package javathread1;
  *
  * @author bianchi.emmanuele
  */
-public class JavaThread1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Processo T1 = new Processo("T1");
-        Processo T2 = new Processo("T2");
+public class Processo extends Thread {
+    String a;
+    public Processo(String a){
+        this.a = a;
+        this.start();
     }
-
+    
+    public void run(){
+       for(;;) System.out.println(a); 
+    }
 }
+
+
