125c98aaebcf9412034814cc9bfa857194213504
bianchi.emmanuele <bianchi.emmanuele@TAVE-PC33.OSDBSESTO.local>
2020-11-24 12:39:49 +0100

    Init Repo

---
+++ src/javathread1/JavaThread1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread1;
+
+/**
+ *
+ * @author bianchi.emmanuele
+ */
+public class JavaThread1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
